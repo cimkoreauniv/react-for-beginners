41e3a83820a6f95e41bed074ccd62099d48c9ea3
cimkoreauniv <changilmoon@korea.ac.kr>
2022-07-12 21:42:38 +0900

    Making To-Do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue(x => x + 1);
-  console.log("all time");
-  useEffect(() => console.log("only once"), []);
-  useEffect(() => {
-    if(keyword)
-      console.log("searching...");
-  }, [keyword]);
+  const [toDo, setToDo] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  function onSubmit(event) {
+    event.preventDefault();
+    if (toDo === "")
+      return;
+    setToDo("");
+    setToDoList(prev => [toDo, ...prev]);
+  }
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={event => setKeyword(event.target.value)}
-        type="text"
-        placeholder="Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1>My To Dos ({toDoList.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={event => setToDo(event.target.value)} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDoList.map((item, idx) => <li key={idx}>{item}</li>)}
+      </ul>
     </div>
   );
 }
